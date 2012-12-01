86c88ea19d13d4753b748f861517ea80fc225f8a
Odin Dutton <odindutton@gmail.com>
2012-12-01 10:06:58 +1000

    Move scrape rake task into a method on FreshSearchApi.

--- Rakefile
+++ Rakefile
@@ -1,15 +1,6 @@
 # This task is called by the Heroku scheduler add-on
 desc 'Scrape freshshell/fresh/wiki/Directory'
 task :scrape do
-  require 'nokogiri'
-  require 'open-uri'
-
-  doc = Nokogiri::HTML(open('https://github.com/freshshell/fresh/wiki/Directory'))
-  doc.css('#wiki-body .markdown-body li').each do |li|
-    fresh_line = li.css('code').text
-    description = li.css('a').text
-    link = li.css('a').attr('href').value
-
-    puts "`#{fresh_line}` # #{description} - #{link}"
-  end
+  require './fresh_search_api.rb'
+  FreshSearchApi.scrape
 end
--- fresh_search_api.rb
+++ fresh_search_api.rb
@@ -3,10 +3,23 @@ ENV['RACK_ENV'] ||= 'development'
 require 'bundler'
 Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 
+require 'open-uri'
 require 'sinatra/reloader' if Sinatra::Base.development?
 
 class FreshSearchApi < Sinatra::Base
   get '/' do
     #
+
+  private
+
+  def self.scrape
+    doc = Nokogiri::HTML(open('https://github.com/freshshell/fresh/wiki/Directory'))
+    doc.css('#wiki-body .markdown-body li').each do |li|
+      fresh_line = li.css('code').text
+      description = li.css('a').text
+      link = li.css('a').attr('href').value
+
+      puts "`#{fresh_line}` # #{description} - #{link}"
+    end
   end
 end
