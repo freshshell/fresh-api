05a02cc3bf6364f7a151db655cbcdbe58a76f5e1
Odin Dutton <odindutton@gmail.com>
2012-12-01 10:08:05 +1000

    Add caching.

--- fresh_search_api.rb
+++ fresh_search_api.rb
@@ -7,19 +7,24 @@ require 'open-uri'
 require 'sinatra/reloader' if Sinatra::Base.development?
 
 class FreshSearchApi < Sinatra::Base
+  set :cache, Dalli::Client.new
+
   get '/' do
-    #
+    settings.cache.get('lines')
+  end
 
   private
 
   def self.scrape
+    lines = []
     doc = Nokogiri::HTML(open('https://github.com/freshshell/fresh/wiki/Directory'))
     doc.css('#wiki-body .markdown-body li').each do |li|
       fresh_line = li.css('code').text
       description = li.css('a').text
       link = li.css('a').attr('href').value
 
-      puts "`#{fresh_line}` # #{description} - #{link}"
+      lines << "`#{fresh_line}` # #{description} - #{link}"
     end
+    settings.cache.set('lines', lines.join("\n"))
   end
 end
